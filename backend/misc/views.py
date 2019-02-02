78d34b6940db38d15e77507bbbf29ba493c06ad4
yukun <yykcool@hotmail.com>
2019-02-02 12:30:03 +0800

    remove capitalization

--- backend/misc/views.py
+++ backend/misc/views.py
@@ -3,6 +3,6 @@ from rest_framework.views import APIView
 from rest_framework import authentication, permissions
 
 
-class Webhook_test(APIView):
+class webhook_test(APIView):
     def post (self, request):
         print(request)
