bfca6a1c54512c7beeeca0b3528642a71485e11f
yukun <yykcool@hotmail.com>
2019-02-03 06:14:48 +0800

    added simple getters

--- node/routes/default.js
+++ node/routes/default.js
@@ -1,9 +1,72 @@
 const express = require('express');
 const router = express.Router();
 
+const {Client} = require('pg');
+const client = new Client();
+
+client.connect()
+
 //homepage
 router.get('/', (req, res)=>{
-    console.log('lol')
+
+    
+    const text = 'SELECT COUNT(*) FROM handle_help';
+
+    var handle_help = 0;
+
+    client.query(text,values,(err,res) => {
+        if(err){
+            console.log(err.stack);
+        } else {
+            handle_help = res.rows[0];
+        }
+    });
+
+    text = 'SELECT COUNT(*) FROM handle_criminal_law';
+
+    var handle_crim = 0;
+
+    client.query(text,values,(err,res) => {
+        if(err){
+            console.log(err.stack);
+        } else {
+            handle_crim = res.rows[0];
+        }
+    });
+
+    text = 'SELECT COUNT(*) FROM handle_personal_injury';
+
+    var handle_injury = 0;
+
+    client.query(text,values,(err,res) => {
+        if(err){
+            console.log(err.stack);
+        } else {
+            handle_injury = res.rows[0];
+        }
+    });
+
+    text = 'SELECT COUNT(*) FROM handle_unfair_contracts';
+
+    var handle_unfair = 0;
+
+    client.query(text,values,(err,res) => {
+        if(err){
+            console.log(err.stack);
+        } else {
+            handle_unfair = res.rows[0];
+        }
+    });
+
+    var result = {
+        "criminal law": handle_crim,
+        "personal injury law": handle_injury,
+        "worker's rights law": handle_help,
+        "unfair contract law": handle_unfair
+    };
+
+    res.send(result);
+    
 });
 
 module.exports = router;
