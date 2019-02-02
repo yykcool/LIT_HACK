dae10b308944f8f3619d7268f04e7e9937ca9c86
yukun <yykcool@hotmail.com>
2019-02-03 03:48:31 +0800

    asdasdasdasd

--- node/routes/exports/psql.js
+++ node/routes/exports/psql.js
@@ -1,14 +1,7 @@
 const pgp = require('pg-promise');
 
-const cn = {
-    host: '127.0.0.1:5432',
-    port: 5432,
-    database: 'root',
-    user: 'root',
-    password: 'password'
-};
 
-const db = pgp(cn);
+const db = pgp('postgres://root:password@localhost:5432/root');
 
 //export function that can be treated like class
 module.exports.put = function(table, relation){
