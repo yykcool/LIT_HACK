96e90ebebfa80ea9413e353469b064c596e4b3a6
yukun <yykcool@hotmail.com>
2019-02-02 12:41:39 +0800

    added response

--- backend/misc/views.py
+++ backend/misc/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework import authentication, permissions
-
+from rest_framework.views import APIView, Response
+import json
 
 class webhook_test(APIView):
     def post (self, request):
         print(request)
+        return Response({'status_code':'200'})
+
